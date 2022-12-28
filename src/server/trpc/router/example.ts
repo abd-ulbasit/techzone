e8f59441571cd84eeae2a0749aa93959b2674585
abd-ulbasit <basit01035@gmail.com>
2022-12-28 21:08:03 +0500

    made the database schema

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -10,7 +10,26 @@ export const exampleRouter = router({
         greeting: `Hello ${input?.text ?? "world"}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // return await ctx.prisma
+    //   .$queryRaw`select * from Session where expires > "2022-02-02"`;
+
+    return await ctx.prisma.session.findMany({
+      where: { expires: { gt: new Date("2022-02-02") } },
+    });
   }),
+  getUser: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.user.findMany();
+  }),
+  getuserwithname: publicProcedure
+    .input(
+      z
+        .object({
+          name: z.string().optional(),
+        })
+        .optional()
+    )
+    .query(async ({ ctx }) => {
+      return await ctx.prisma.account.findMany();
+    }),
 });
