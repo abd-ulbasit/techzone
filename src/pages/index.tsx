804a7cc7ed8b676d912a35959949ed40e8ed1619
abd-ulbasit <basit01035@gmail.com>
2023-06-17 23:37:47 +0500

    add daisyui and remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 // import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import ProductCard from "../components/UI/ProductCard";
 const Home: NextPage = () => {
