f3ca248efadc5e447d29b31efb5878ca91a4db57
abd-ulbasit <basit01035@gmail.com>
2023-06-18 11:43:33 +0500

    add _document.tsx file to add theme to document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html data-theme="retro" >
+                <Head>
+                    {/* Add your HTML tag modifications here */}
+                    {/* For example, add a meta tag */}
+                    <meta name="description" content="My custom description" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+export default MyDocument;
