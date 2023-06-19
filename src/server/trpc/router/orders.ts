fa831afd1a9776e8eeb2372ff171d8f570611748
abd-ulbasit <basit01035@gmail.com>
2023-06-19 19:37:20 +0500

    add order muation on server

--- src/server/trpc/router/orders.ts
+++ src/server/trpc/router/orders.ts
@@ -1,6 +1,7 @@
 // import { Cart } from '@prisma/client';
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
+import type { Cart } from "@prisma/client";
 // import Cart from '../../../pages/Cart';
 // import { randomInt } from "crypto";
 
@@ -37,6 +38,33 @@ export const ordersRouter = router({
         where: {
           user_id: input.user_id,
         },
+      });
+    }),
+  addOrder: publicProcedure
+    .input(
+      z.unknown().refine((data): data is Cart[] => {
+        return Array.isArray(data);
+      }) as z.ZodType<Cart[]>
+    )
+    .mutation(async ({ ctx, input }) => {
+      //first register an order in orders table
+      const order = await ctx.prisma.orders.create({
+        data: {
+          user_id: input[0]?.user_id as string,
+          order_date: new Date(),
+        },
+      });
+      const order_id = order.order_id;
+      const order_detail = input.map((item) => {
+        return {
+          order_id: order_id,
+          product_id: item.product_id,
+          product_quantity: item.product_quantity,
+        };
+      });
+      //add all theses items in order_details table
+      await ctx.prisma.order_detail.createMany({
+        data: order_detail,
       });
     }),
 });
