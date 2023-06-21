9f77c9ed739ba827907fa4380e3aa2e24073777a
abd-ulbasit <basit01035@gmail.com>
2023-06-21 17:03:20 +0500

    limit no of items in cart from cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -11,6 +11,7 @@ const Cart = () => {
     const user_id = userSession?.user?.id ?? "";
     //this cart will have all the products fethced with in it
     const { data: cart } = trpc.cart.getCartWithProducts.useQuery({ user_id: user_id })
+    const { data: productsFromInventory } = trpc.products.getProductsWithDetails.useQuery(cart?.map((item) => item.product_id) ?? [])
     const trpcContext = trpc.useContext();
 
     // console.log(cart);
@@ -38,7 +39,13 @@ const Cart = () => {
         }
     })
     // const cartType = typeof cart
-    const handleIncermentinCart = (product_id: number) => {
+    const handleIncermentinCart = (product_id: number, productQuantity: number) => {
+        if (!cart) return;
+        // const { data: product } = trpc.products.getProductwithDetails.useQuery({ product_id })
+        if (productQuantity === productsFromInventory?.find((item) => item.pid === product_id)?.quanity_in_inventory) {
+            toast.error("you can not add more items")
+            return;
+        }
         incrementIncartMutation.mutate({
             user_id: user_id, product_id: product_id
         })
@@ -80,7 +87,7 @@ const Cart = () => {
                                 <div className="card-actions justify-end flex items-center">
                                     <button onClick={() => handleDecremnetIncart(item.product_id)} className='btn btn-ghost' ><Image src={"/static/remove_FILL0_wght400_GRAD0_opsz48.svg"} alt="-" width={30} height={30} /></button>
                                     <h2 className='font-semibold text-xl ' >{item.product_quantity}</h2>
-                                    <button className='btn btn-ghost' onClick={() => handleIncermentinCart(item.product_id)} ><Image src={"/static/add_FILL0_wght400_GRAD0_opsz48.svg"} alt="+" width={30} height={30} /></button>
+                                    <button className='btn btn-ghost' onClick={() => handleIncermentinCart(item.product_id, item.product_quantity)} ><Image src={"/static/add_FILL0_wght400_GRAD0_opsz48.svg"} alt="+" width={30} height={30} /></button>
                                 </div>
 
                             </div>
@@ -88,11 +95,14 @@ const Cart = () => {
                     }) : ""
                 }
             </div>
-            {userSession &&
+            {userSession && grandtotal ?
                 <div className=' fixed right-12 bottom-12' >
                     <div className='text-center  bg-primary text-base-300 py-2 ' >{grandtotal} PKRs</div>
-                    <Link href={`/CheckOut`} className=' p-3 rounded-sm hover:scale-105  hover:rounded-md btn' >Proceed to Checkout </Link>
-                </div>}
+                    <Link href={`/CheckOut`} className=' p-3 rounded-sm hover:scale-105  hover:rounded-md btn'  >Proceed to Checkout </Link>
+                </div> : ""}
+            {!grandtotal && <div className=' fixed right-12 bottom-12' >
+                <div className='text-center  bg-primary text-base-300 py-2 px-6' >Cart Is Empty</div>
+            </div>}
         </div>
     )
 }
