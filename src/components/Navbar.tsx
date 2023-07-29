2b0690e93f8c583c6e68e0ed7b84be01950afe03
abd-ulbasit <basit01035@gmail.com>
2023-07-29 22:08:35 +0500

    fix catagoires link issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,15 +36,14 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
                         </div>
                         <div className="drawer-side z-20 ">
                             <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                            <ul className="menu p-4 w-80 h-full text-base-content bg-base-100 flex flex-col">
+                            <div className="menu p-4 w-80 h-full text-base-content bg-base-100 flex flex-col">
 
                                 {catogoryData ? catogoryData.map((category) => {
-                                    return <li key={category.id} className={" btn btn-outline my-1 "} >
-                                        <Link href={`/categories/${category.category_name}`} >
-                                            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                                            {category.category_name}
-                                        </Link>
-                                    </li>
+
+                                    return <Link className='btn btn-outline mb-1' href={`/categories/${category.category_name}`} key={category.id} >
+                                        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                                        {category.category_name}
+                                    </Link>
                                 }) : ""
                                 }
                                 <div className='flex flex-grow items-end content-center' >
@@ -53,7 +52,7 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
                                         {theme}
                                     </button>
                                 </div>
-                            </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -65,7 +64,7 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item">{noOfItemsInCart}</span>
+                                <span className="badge badge-sm indicator-item ">{noOfItemsInCart}</span>
                             </div>
                         </label>
                         <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow z-10">
