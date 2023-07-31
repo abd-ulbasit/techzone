a85f92c03eaabde4ec2aa4cacba4a67bdc6af963
abd-ulbasit <basit01035@gmail.com>
2023-07-31 16:54:12 +0500

    validate count in cart is less than count in stock

--- src/server/trpc/router/cart.ts
+++ src/server/trpc/router/cart.ts
@@ -41,6 +41,24 @@ export const cartRouter = router({
       })
     )
     .mutation(async ({ ctx, input }) => {
+      //chech if product count in cart is less than product count in product table
+      const product = await ctx.prisma.product.findUnique({
+        where: {
+          pid: input.product_id,
+        },
+      });
+      const cart = await ctx.prisma.cart.findUnique({
+        where: {
+          user_id_product_id: {
+            user_id: input.user_id,
+            product_id: input.product_id,
+          },
+        },
+      });
+      if (cart?.product_quantity == product?.quanity_in_inventory) {
+        return cart;
+      }
+
       return await ctx.prisma.cart.update({
         where: {
           user_id_product_id: {
