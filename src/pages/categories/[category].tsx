3653a44a003628372cee4364e4474cd81c06220c
abd-ulbasit <basit01035@gmail.com>
2023-08-02 18:56:15 +0500

    use product state in categories

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -1,19 +1,11 @@
 import { useRouter } from 'next/router'
 import React from 'react'
-import { trpc } from '../../utils/trpc';
-// import Image from 'next/image';
 import ProductCard from '../../components/UI/ProductCard';
+import useProductStore from '../../stores/productStore';
 const Category = () => {
     const router = useRouter();
-    // const { category }:{category:string} = router.query.;
-    const category = router.query.category
-    const aCategory = category as string;
-    const { data } = trpc.categories.getProductbyCategory.useQuery({ category: aCategory });
-    console.log(category);
-    const products = data?.product;
-    // console.log(products);
-
-
+    const categoryId = parseInt(router.query.category as string)
+    const products = useProductStore(({ products }) => products).filter((product) => product.category_Id === categoryId)
 
     return (
         <div className="flex gap-3 flex-wrap  justify-center items-start " >
