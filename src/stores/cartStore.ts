0b2ef652888136c0cd0a06bd47f0e444d70e4bd2
abd-ulbasit <basit01035@gmail.com>
2023-08-03 21:16:17 +0500

    add cart store with zustand

---
+++ src/stores/cartStore.ts
@@ -0,0 +1,56 @@
+import type { Product, Cart } from "@prisma/client";
+import { create } from "zustand";
+interface CartItem extends Cart {
+  product: Product;
+}
+interface CartStoreType {
+  cart: CartItem[];
+  setCart: (cart: CartItem[]) => void;
+  addToCart: (product: CartItem) => void;
+  removeFromCart: (productId: number) => void;
+  increaseQuantity: (productId: number) => void;
+  deceaseQuantity: (productId: number) => void;
+  clearCart: () => void;
+}
+export const useCartStore = create<CartStoreType>((set) => ({
+  cart: [],
+  setCart: (cart: CartItem[]) => set({ cart }),
+  addToCart: (cartItem: CartItem) =>
+    set((state) => ({
+      cart: [...state.cart, cartItem],
+    })),
+  SetCart: (cart: CartItem[]) => set({ cart }),
+  removeFromCart: (productId: number) =>
+    set((state) => ({
+      cart: state.cart.filter((item) => item.product.pid !== productId),
+    })),
+  clearCart: () => set({ cart: [] }),
+  increaseQuantity: (productId: number) =>
+    set((state) => ({
+      // console.log("increaseQuantity")
+
+      cart: state.cart.map((item) => {
+        console.log("item.product.pid", item.product.pid);
+
+        if (item.product.pid === productId) {
+          return {
+            ...item,
+            quantity: item.product_quantity + 1,
+          };
+        }
+        return item;
+      }),
+    })),
+  deceaseQuantity: (productId: number) =>
+    set((state) => ({
+      cart: state.cart.map((item) => {
+        if (item.product.pid === productId) {
+          return {
+            ...item,
+            quantity: item.product_quantity - 1,
+          };
+        }
+        return item;
+      }),
+    })),
+}));
